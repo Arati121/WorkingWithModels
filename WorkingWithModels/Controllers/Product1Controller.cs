cf3dcb1ccee46c444d35363abc974d657addd618
Arati121 <avbhosale@mitaoe.ac.in>
2022-04-20 00:43:40 +0530

    p

---
+++ WorkingWithModels/Controllers/Product1Controller.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithModels.Models;
+
+namespace WorkingWithModels.Controllers
+{
+    public class Product1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult List(Product1 p)
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product1 p)
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Product1 p)
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(Product1 p)
+        {
+            return View();
+        }
+    }
+}
---
+++ WorkingWithModels/Models/Product1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithModels.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace WorkingWithModels.Models
+{
+    public class Product1
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [DisplayName("Product name*")]
+        [DataType(DataType.Text)]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Price*")]
+        [Required(ErrorMessage = "Please enter Price")]
+        [DataType(DataType.Text)]
+        public int Price { get; set; }
+
+        [Required]
+        [DisplayName("Product Quantity")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [DisplayName("Date And Time")]
+        [DataType(DataType.DateTime)]
+
+        public DateTime Ptime { get; set; }
+    }
+}
